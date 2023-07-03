a93b5a3a8cf7fece6dd00a49aa8e028cf9a0eaff
khaled <kha>
2023-07-04 01:50:57 +0300

    save variables to env

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -10,20 +10,12 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORGE_BUKET,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_API_ID,
+  appId: import.meta.env.VITE_APP_ID,
   measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB9cKlnuOkKAUFwcjp97MrqG2Ghj5llUzU",
-//   authDomain: "auth-app-6152d.firebaseapp.com",
-//   projectId: "auth-app-6152d",
-//   storageBucket: "auth-app-6152d.appspot.com",
-//   messagingSenderId: "227637409769",
-//   appId: "1:227637409769:web:ce019f79cc3b0960833d78",
-//   measurementId: "G-J6WPCM1QGX"
-// };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
